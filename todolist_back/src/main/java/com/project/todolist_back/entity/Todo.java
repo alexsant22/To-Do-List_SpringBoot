90e3fc56d82722f7de3b976542bf176b612cf055
Alexandre Santos <alexandre.santana2201@gmail.com>
2025-09-09 15:27:03 -0300

    Entity - Att

--- todolist_back/src/main/java/com/project/todolist_back/entity/Todo.java
+++ todolist_back/src/main/java/com/project/todolist_back/entity/Todo.java
@@ -1,6 +1,7 @@
 package com.project.todolist_back.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,7 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O nome é obrigatório.")
     @Column(nullable = false)
     private String nome;
 
